aaef5cfe3d726eff212231f9cb039dc483733f32
Jonathan <jonathanfernandezfm@gmail.com>
2021-03-03 17:40:18 +0100

    Added timeout

--- src/api/graph-ethermine.js
+++ src/api/graph-ethermine.js
@@ -20,27 +20,32 @@ module.exports = {
 
 			const selector = 'svg.highcharts-root';
 			await page.waitForSelector(selector);
-			const rect = await page.evaluate((selector) => {
-				const element = document.querySelector(selector);
-				if (!element) return null;
-				const { x, y, width, height } = element.getBoundingClientRect();
-				return { left: x, top: y, width, height, id: element.id };
-			}, selector);
 
-			if (!rect) throw Error(`Could not find element that matches selector: ${selector}.`);
+			return new Promise(async (resolve, reject) => {
+				setTimeout(async () => {
+					const rect = await page.evaluate((selector) => {
+						const element = document.querySelector(selector);
+						if (!element) return null;
+						const { x, y, width, height } = element.getBoundingClientRect();
+						return { left: x, top: y, width, height, id: element.id };
+					}, selector);
 
-			const image = await page.screenshot({
-				clip: {
-					x: rect.left,
-					y: rect.top,
-					width: rect.width,
-					height: rect.height,
-				},
+					if (!rect) throw Error(`Could not find element that matches selector: ${selector}.`);
+
+					const image = await page.screenshot({
+						clip: {
+							x: rect.left,
+							y: rect.top,
+							width: rect.width,
+							height: rect.height,
+						},
+					});
+
+					await browser.close();
+
+					resolve(image);
+				}, 1000);
 			});
-
-			await browser.close();
-
-			return image;
 		} catch (err) {
 			throw err;
 		}
