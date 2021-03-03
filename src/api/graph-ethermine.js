4619eb3cdc31d0f0f48495989290303bbdbdfd1c
Jonathan <jonathanfernandezfm@gmail.com>
2021-03-03 16:38:02 +0100

    Args to puperteer

--- src/api/graph-ethermine.js
+++ src/api/graph-ethermine.js
@@ -6,7 +6,7 @@ module.exports = {
 		try {
 			const url = req.query.url;
 
-			const browser = await puppeteer.launch();
+			const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 			const page = await browser.newPage();
 			await page.goto(url);
 
